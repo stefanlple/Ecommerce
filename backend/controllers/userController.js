2ddff63510efc9aed9a25a43378e9eff9d296ea0
Stefan Le <stefanle01@yahoo.de>
2022-08-22 21:27:32 +0200

    fdsg

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
   }
   res.json({ message: "registerUser" });
 };
