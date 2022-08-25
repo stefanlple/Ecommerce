c10a6661506f0c88d23afa92b17382db3c517777
Stefan Le <stefanle01@yahoo.de>
2022-08-26 00:07:18 +0200

    finish connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
