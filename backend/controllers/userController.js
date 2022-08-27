2f9199bf82f83d9139a4176a53a1c1c03a89bd5c
Stefan Le <stefanle01@yahoo.de>
2022-08-27 23:51:09 +0200

    login and register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,51 @@
+const User = require("../models/userModel");
+
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
+
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please add all fields");
   }
-  console.log(name, email);
-  res.json({ message: "registerUser" });
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+  if (user) {
+    res.status(201);
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    });
+  }
 };
 
-const loginUser = (req, res) => {
-  res.json({ message: "loginUser" });
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && password == user.password) {
+    res.status(201);
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Wrong Password or Email");
+  }
 };
 
 module.exports = { registerUser, loginUser };
