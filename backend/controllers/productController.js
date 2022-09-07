9c0427a2d56a00d9f5a5effd0a97b96bc6bf8de8
Stefan Le <stefanle01@yahoo.de>
2022-09-07 23:57:30 +0200

    add product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,51 @@
 const Product = require("../models/productModel");
 
 const registerProduct = (req, res) => {
-  res.json({
-    message: "asdfaw",
+  const {
+    name,
+    category,
+    color,
+    description,
+    quantity,
+    price,
+    isActive,
+    soldOut,
+  } = req.body;
+  const images = req.body.file;
+
+  const productExist = Product.findOne({ name, color });
+  if (productExist) {
+    res.status(400);
+    throw new Error("Product already exist");
+  }
+
+  const product = Product.create({
+    name: name,
+    category: category,
+    color: color,
+    description: description,
+    quantity: quantity,
+    price: price,
+    isActive: isActive,
+    soldOut: soldOut,
+    images: images,
   });
+
+  if (product) {
+    res.status(200);
+    res.json({
+      _id: product.id,
+      name: product.name,
+      category: product.category,
+      color: product.color,
+      description: product.description,
+      quantity: product.quantity,
+      price: product.price,
+      isActive: product.isActive,
+      soldOut: product.soldOut,
+      images: product.images,
+    });
+  }
 };
 
 const getProduct = (req, res) => {
