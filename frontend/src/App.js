f32771c8b263a89bd0251a1fb48226b4711b8950
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2022-09-21 18:19:19 +0200

    basic frontend routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+
 function App() {
   return (
-    <div className="App">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
