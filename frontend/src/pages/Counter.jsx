1fd3936b14e4b186df5f07c563a6c5c9913f8206
Stefan Le <stefanle01@yahoo.de>
2023-03-19 15:49:30 +0100

    added counter

--- frontend/src/pages/Counter.jsx
+++ frontend/src/pages/Counter.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 function Counter() {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount((prevCount) => {
+      if (prevCount < 9) return ++prevCount;
+    });
+  };
+  const decrement = () => {
+    setCount((prevCount) => {
+      if (prevCount > 0) return --prevCount;
+    });
+  };
+
   return (
     <>
-      <div className="inline-block">
-        <button>+</button>
-        <h1>1</h1>
-        <button>-</button>
+      <div className="flex justify-center space-x-4 ">
+        <button className="text-lg hover:scale-125" onClick={decrement}>
+          -
+        </button>
+        <input
+          value={count}
+          type="text"
+          className="w-10 h-10 text-center text-lg"
+          maxlength="1"
+          pattern="\d*"
+          oninput="this.value=this.value.replace(/[^0-9]/g,'');"
+        ></input>
+        <button className="text-lg hover:scale-125" onClick={increment}>
+          +
+        </button>
       </div>
     </>
   );
