ac9581b85b8932c4107ee9d7e21d195a7753d6d1
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-04-10 18:04:20 +0200

    added sizes select table

--- frontend/src/components/ImageUpload.jsx
+++ frontend/src/components/ImageUpload.jsx
@@ -40,10 +40,7 @@ function ImageUpload() {
   return (
     <div className="border border-stone-400 p-2">
       <div className="flex flex-col gap-2 mb-2">
-        <label
-          for="files"
-          class="m-auto btn border border-black p-3 font-semibold"
-        >
+        <label for="files" class="m-auto border border-black p-3 font-semibold">
           Select Images and Model <FaRegPlusSquare className="inline m-auto " />
         </label>
         <input
@@ -56,25 +53,44 @@ function ImageUpload() {
         <p>Selected file count: {images.length}</p>
       </div>
       <div className="flex gap-2">
-        {images.map((image, index) => (
-          <div
-            className="flex flex-col items-center p-2 border border-black gap-3"
-            key={index}
-            draggable
-            onDragStart={(e) => handleDragStart(e, index)}
-            onDragOver={handleDragOver}
-            onDrop={(e) => handleDrop(e, index)}
-          >
-            <p>Image {index + 1}</p>
-            <img src={image} alt="uploaded" width="100" height="100" />
-            <button
-              className="mt-auto"
-              onClick={() => handleRemoveImage(index)}
+        {images.length ? (
+          images.map((image, index) => (
+            <div
+              className="flex flex-col items-center p-2 border border-black gap-3"
+              key={index}
+              draggable
+              onDragStart={(e) => handleDragStart(e, index)}
+              onDragOver={handleDragOver}
+              onDrop={(e) => handleDrop(e, index)}
             >
-              Remove
-            </button>
-          </div>
-        ))}
+              <p>Image {index + 1}</p>
+              <img src={image} alt="uploaded" width="100" height="100" />
+              <button
+                className="mt-auto"
+                onClick={() => handleRemoveImage(index)}
+              >
+                Remove
+              </button>
+            </div>
+          ))
+        ) : (
+          <>
+            <label
+              for="files"
+              class=" font-semibold"
+              className="flex items-center justify-center w-[100px] h-[100px] border border-black"
+            >
+              <FaRegPlusSquare className="inline m-auto " />
+            </label>
+            <input
+              id="files"
+              type="file"
+              className="invisible absolute p-2 border-2"
+              onChange={handleImageChange}
+              multiple
+            />
+          </>
+        )}
       </div>
     </div>
   );
--- frontend/src/components/ProductUpload.jsx
+++ frontend/src/components/ProductUpload.jsx
@@ -1,40 +1,74 @@
 import React from "react";
+import ImageUpload from "./ImageUpload";
 
 function ProductUpload() {
   return (
-    <form action="#" className="flex flex-col">
+    <form action="#" className="flex flex-col w-6/12">
       <h1>UPLOAD PRODUCT</h1>
+      <ImageUpload />
       <label for="name">Enter name:</label>
       <input type="text" id="name" className="border-2" placeholder="NAME" />
-      <label for="currency">Enter currency:</label>
+      <label for="price">Enter price:</label>
       <input
         type="text"
-        id="currency"
-        name="currency"
+        id="price"
+        name="price"
         pattern="(€|\$)\d+(\.\d{2})?"
         placeholder="PRICE"
+        className="border-2"
         required
       />
+
       <label for="category">Enter category:</label>
-      <input
-        type="text"
-        className="border-2"
-        id="category"
-        placeholder="CATEGORY"
-      />
-      <label for="size">Enter currency:</label>
-      <input type="text" id="size" className="border-2" placeholder="SIZE" />
+      <select name="category" id="category" className="border-2">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+
       <label for="color">Enter colors:</label>
-      <input type="text" className="border-2" placeholder="COLOR" />
-      <label for="img">Enter images:</label>
-      <input
-        type="file"
-        id="img"
-        name="filesfield"
-        className="border-2"
-        multiple
-      />
-      <input type="submit" className="border-2" />
+      <input id="color" type="text" className="border-2" placeholder="COLOR" />
+
+      <span>Sizing:</span>
+      <table className="border border-black border-collapse">
+        <thead className="border border-black border-collapse">
+          <tr>
+            <th className="border border-black border-collapse">
+              <label for="size">Enter size:</label>
+              <select name="size" id="size" className="border-2">
+                <option value="onesize">One Size</option>
+                <optgroup label="Sizes">
+                  <option value="xxs">XXS</option>
+                  <option value="xs">XS</option>
+                  <option value="s">S</option>
+                  <option value="m">M</option>
+                  <option value="l">L</option>
+                  <option value="xl">XL</option>
+                  <option value="xxl">XXL</option>
+                </optgroup>
+              </select>
+            </th>
+            <th className="border border-black border-collapse">
+              <label for="quantity">Enter quantity:</label>
+              <input
+                type="text"
+                id="quantity"
+                className="border-2"
+                placeholder="QUANTITY"
+              />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border border-black border-collapse">1</td>
+            <td className="border border-black border-collapse">2</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <input type="submit" className="border-2 bg-black text-white mt-5 p-2" />
     </form>
   );
 }
