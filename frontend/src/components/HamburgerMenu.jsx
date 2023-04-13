30bdc6d68b669fab4efbe6695deb5bf2252b41d4
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-04-13 12:43:41 +0200

    burger animation

--- frontend/src/components/HamburgerMenu.jsx
+++ frontend/src/components/HamburgerMenu.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function HamburgerMenu() {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen((curr) => !curr);
+  };
+
   return (
     <nav>
-      <div className="flex items-center justify-center w-8 h-8 bg-black">
+      <div
+        className="flex items-center justify-center w-8 h-8"
+        onClick={handleOpen}
+      >
         <div
-          class="w-6 h-0.5 rounded-lg bg-white
-          before:content-[''] before:absolute before:w-6 before:h-0.5 before:rounded-lg before:bg-white transform before:-translate-y-2"
+          className={`w-6 h-0.5 rounded-3xl bg-black
+          before:content-[''] before:absolute before:w-6 before:h-0.5 before:rounded-rounded-3xl before:bg-black before:-translate-y-1.5 before:-translate-x-1/2 
+          after:content-[''] after:absolute after:w-6 after:h-0.5 after:rounded-rounded-3xl after:bg-black after:translate-y-1.5 after:-translate-x-1/2
+          before:transition-transform before:duration before:ease-in-out
+          after:transition-transform after:duration after:ease-in-out
+          ${
+            isOpen &&
+            "bg-transparent before:rotate-45 before:translate-y-0 after:-rotate-45 after:-translate-y-0"
+          }`}
         ></div>
       </div>
     </nav>
