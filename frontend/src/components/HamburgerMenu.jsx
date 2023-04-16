5b262537e57e630f2d2859cfc025b0a5f24a8d57
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-04-16 13:57:48 +0200

    finished burger menu

--- frontend/src/components/HamburgerMenu.jsx
+++ frontend/src/components/HamburgerMenu.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 function HamburgerMenu() {
   const [isOpen, setOpen] = useState(false);
@@ -23,15 +24,103 @@ function HamburgerMenu() {
             isOpen &&
             `bg-transparent 
             before:bg-slate-50 before:rotate-45 before:translate-y-0 
-            after:bg-slate-50 after:-rotate-45 after:-translate-y-0`
+            after:bg-slate-50 after:-rotate-45 after:translate-y-0`
           }`}
         ></span>
       </div>
       <nav
-        className={`fixed  ${
-          isOpen ? "w-full" : "w-0"
-        } h-full top-0 left-0 bg-neutral-900 bg-opacity-70 -z-10 transition-all duration-500 east-in-out`}
-      ></nav>
+        className={`fixed text-left pt-24 overflow-hidden
+        ${isOpen ? "w-full" : "w-0"} 
+        h-full top-0 left-0 bg-neutral-900 bg-opacity-70 backdrop-blur-md -z-10 transition-all duration-500 east-in-out`}
+      >
+        <div className="px-5 h-5/6 overflow-y-scroll overflow-x-hidden">
+          <div className="flex flex-col pb-3 gap-3">
+            <a href="#" className="text-white uppercase font-thin">
+              news
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              tees
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              knitwear
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              top
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              outerwear
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              pants
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              shorts
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              accessoires
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              footwear
+            </a>
+            <a href="#" className="text-white uppercase font-thin">
+              special
+            </a>
+          </div>
+          <form
+            action=""
+            className="pb-2 border-b-white border-b-[1px] relative"
+          >
+            <input
+              type="text"
+              name=""
+              id=""
+              className="w-full h-8 bg-transparent text-white font-thin outline-none text-sm"
+              placeholder="SEARCH"
+            />
+            <button
+              type="submit"
+              className="absolute right-2 p-0 translate-y-1/2 text-white"
+            >
+              <FaSearch />
+            </button>
+          </form>
+          <div className="flex flex-col gap-2 mt-3 pb-3">
+            <a href="" className="text-white font-extralight text-xs">
+              Support
+            </a>
+            <a href="" className="text-white font-extralight text-xs">
+              Terms & Conditions
+            </a>
+            <a href="" className="text-white font-extralight text-xs">
+              Privacy Policy
+            </a>
+            <a href="" className="text-white font-extralight text-xs">
+              Instagram
+            </a>
+          </div>
+          <form
+            action=""
+            className="py-2 border-b-white border-b-[1px] relative"
+          >
+            <label for="newsletter" className="text-white font-thin text-xs">
+              NEWSLETTER
+            </label>
+            <input
+              type="text"
+              name="newsletter"
+              id="newsletter"
+              className="w-full h-8 bg-transparent text-white font-thin outline-none text-xs"
+              placeholder="Enter your Email"
+            />
+            <button
+              type="submit"
+              className="absolute right-2 p-0 translate-y-1/2 text-white font-thin text-xs"
+            >
+              SUBSCRIBE
+            </button>
+          </form>
+        </div>
+      </nav>
     </>
   );
 }
