4068fb36e648e53eee5f6e38228b2bc3d09eb795
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-04-19 18:00:36 +0200

    added nav users and admin

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -5,6 +5,8 @@ function Admin() {
   return (
     <nav>
       <Link to="">DashBoard</Link>
+      <Link to="">Orders</Link>
+      <Link to="">Stock</Link>
       <Link to="">Products</Link>
       <Link to="">Users</Link>
     </nav>
---
+++ frontend/src/pages/Users.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Users() {
+  return (
+    <nav>
+      <Link to="">Order History</Link>
+      <Link to="">Addresses</Link>
+      <Link to="">Account Details</Link>
+    </nav>
+  );
+}
+
+export default Users;
