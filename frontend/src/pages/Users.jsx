e489a9f9c13316db611ce5a9579cf64892d65afa
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-04-30 12:28:11 +0200

    user page styling

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -44,59 +44,68 @@ function Users() {
               return (
                 <>
                   <h1>Addresses</h1>
-                  <div>
+                  <div className="flex flex-col items-center gap-1">
                     <div>
-                      <h2>Name</h2>
+                      <h4 className="font-semibold">Name:</h4>
                       <span>Bob Meier</span>
                     </div>
                     <div>
-                      <h2>Street</h2>
+                      <h4 className="font-semibold">Street:</h4>
                       <span>adfa</span>
                     </div>
                     <div>
-                      <h2>City</h2>
+                      <h4 className="font-semibold">City:</h4>
                       <span>adfa</span>
                     </div>
                     <div>
-                      <h2>State</h2>
+                      <h4 className="font-semibold">State:</h4>
                       <span>adfa</span>
                     </div>
                     <div>
-                      <h2>Country</h2>
+                      <h4 className="font-semibold">Country:</h4>
                       <span>adfa</span>
                     </div>
                     <div>
-                      <h2>Zip Code</h2>
+                      <h4 className="font-semibold">Zip Code:</h4>
                       <span>20303</span>
                     </div>
-                    <button>Edit</button>
+                    <button className="small-standard-button">Edit</button>
                   </div>
                 </>
               );
             case "AccountDetails":
               return (
                 <>
-                  <h1>Account Details</h1>
-                  <h2>Email Address</h2>
-                  <span>bob@bob.de</span>
-                  <h2>First Name</h2>
-                  <span>bob</span>
-                  <h2>Last Name</h2>
-                  <span>meier</span>
-                  <h2>Phone Number</h2>
-                  <span>040 3242304832</span>
-                  <button>Edit</button>
+                  <div className="flex flex-col items-center">
+                    <h1>Account Details</h1>
+                    <h4 className="font-semibold">Email Address</h4>
+                    <span>bob@bob.de</span>
+                    <h4 className="font-semibold">First Name</h4>
+                    <span>bob</span>
+                    <h4 className="font-semibold">Last Name</h4>
+                    <span>meier</span>
+                    <h4 className="font-semibold"> Phone Number</h4>
+                    <span>040 3242304832</span>
+                    <button className="small-standard-button">Edit</button>
+                  </div>
                 </>
               );
             default:
               return (
                 <>
                   <h1>Order History</h1>
-                  <section>
-                    <div>
+                  <section className="flex flex-col gap-1">
+                    <h5 className="font-extrabold">24. Febuary 2023</h5>
+                    <div className="flex flex-col items-center p-1 border border-black">
                       <h5>YOUR ORDER: A234324234</h5>
-                      <span>24 Febuary 2023 | € 234.40 | 1 item</span>
-                      <img src="" alt="altImage" />
+                      <div>24 Febuary 2023 | € 234.40 | 1 item</div>
+                      <button className="small-standard-button">
+                        View Details
+                      </button>
+                    </div>
+                    <div className="flex flex-col items-center p-1 border border-black">
+                      <h5>YOUR ORDER: A234324234</h5>
+                      <div>24 Febuary 2023 | € 234.40 | 1 item</div>
                       <button className="small-standard-button">
                         View Details
                       </button>
